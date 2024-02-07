cc583eabbdc8c17ab4ab6d179b87c6757297fd6f
adamzain99 <adam@arc.market>
2024-02-07 18:02:08 +0800

    update key with anton's

--- server.js
+++ server.js
@@ -10,13 +10,15 @@ app.use(bodyParser.json())
 const port = 9999
 
 // Generated when creating the webhook endpoint in the Radom dashboard
-const verificationKey = "OTUyYjMwMjItYzE4My00ODViLWJmNTgtOTk2ZTU3YTk0MmM1YWUxMzA2ZmItMTY3OC00MzU1LTk0YWItZDRlYzljYjk0MjI1"
+// it is mine
+// const verificationKey = "OTUyYjMwMjItYzE4My00ODViLWJmNTgtOTk2ZTU3YTk0MmM1YWUxMzA2ZmItMTY3OC00MzU1LTk0YWItZDRlYzljYjk0MjI1"
+// it is for anton
+const verificationKey = "MGM2NWEzOWYtMDk2ZS00MjQwLTgyYzctN2JkY2EwMWVlYmQyMWE4NWFjNjYtZjA2Yy00ZDQyLTk1MWMtNTdiMjVmMDc0NDZm"
 
 app.post('/webhook', (req, res) => {
   if (req.headers["radom-verification-key"] != verificationKey) {
     return res.sendStatus(401)
   }
-  console.log("1111111", req.body);
   console.log(util.inspect(req.body, false, null, true /* enable colors */))
 
   res.sendStatus(200)
